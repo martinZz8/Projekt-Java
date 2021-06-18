d52ca14ea97bb54a467a4496499e7cd42301cb73
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2021-06-18 20:06:28 +0200

    up

---
+++ backend/src/main/java/com/example/backend/repositories/ShoppingListRepository.java
@@ -0,0 +1,15 @@
+package com.example.backend.repositories;
+
+import com.example.backend.model.ShoppingList;
+import com.example.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
+    //@Query("SELECT sl FROM shopping_list sl WHERE sl.user.id=?1")
+    List<ShoppingList> getAllByUserId(Long user_id);
+}
